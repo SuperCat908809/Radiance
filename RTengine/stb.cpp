aa4ca95fd5016e21c9be225740ed02a6b9aeef44
SuperCat908809 <57642715+SuperCat908809@users.noreply.github.com>
2025-01-16 21:23:47 +0000

    Added the stb library to load and write images.

---
+++ RTengine/stb.cpp
@@ -0,0 +1,8 @@
+#define __STDC_LIB_EXT1__
+
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include <stb/stb_image.h>
+#include <stb/stb_image_resize2.h>
+#include <stb/stb_image_write.h>
