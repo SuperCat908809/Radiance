a48e63327d54fd83e6c24c7278c021024ed98144
SuperCat908809 <57642715+SuperCat908809@users.noreply.github.com>
2025-01-18 20:45:58 +0000

    Added the Camera_cu class to simplify ray generation. I also now realise that requiring all device code to be compiled from the same translation unit is only required when using virtual methods. Otherwise, its free to mix and match between units.

---
+++ RTengine/camera.h
@@ -0,0 +1,41 @@
+#ifndef CAMERA_CLASS_CUDA_H
+#define CAMERA_CLASS_CUDA_H
+
+#include <cuda_runtime.h>
+#include <glm/glm.hpp>
+#include "ray.h"
+
+namespace RT_ENGINE {
+
+class Camera_cu {
+	glm::vec3 origin, forward, horizontal, vertical;
+
+public:
+
+	__host__ __device__ Camera_cu(glm::vec3 lookfrom, glm::vec3 lookat, glm::vec3 up, float vfov, float aspect_ratio) {
+		float theta = vfov / 2.0f;
+		float viewport_height = tanf(theta) * 2.0f;
+		float viewport_width = viewport_height * aspect_ratio;
+
+		origin = lookfrom;
+		glm::vec3 w = glm::normalize(lookat - lookfrom);
+		glm::vec3 u = glm::normalize(glm::cross(up, w));
+		glm::vec3 v = glm::cross(w, u);
+
+		forward = w;
+		horizontal = u * viewport_width / 2.0f;
+		vertical = v * viewport_height / 2.0f;
+
+		// viewport dimensions are halfed here because the uv inputs are in the range [-1, 1].
+	}
+
+	__host__ __device__ ray sample(float u, float v) const {
+		glm::vec3 d = forward + horizontal * u + vertical * v;
+		return ray(origin, d);
+	}
+
+}; // class Camera_cu //
+
+} // namespace RT_ENGINE //
+
+#endif // ifndef CAMERA_CLASS_CUDA_H //
