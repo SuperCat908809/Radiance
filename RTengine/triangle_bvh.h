d67a308afb91db695537de4ce68cf069217adfbd
SuperCat908809 <57642715+SuperCat908809@users.noreply.github.com>
2025-01-20 20:36:54 +0000

    Removed TriangleBVH_2 since TriangleBVH is fixed now.

--- RTengine/triangle_bvh.h
+++ RTengine/triangle_bvh.h
@@ -37,18 +37,6 @@ class TriangleBVH {
 			return tmax >= tmin && tmin < rec.t && tmax > 0;
 #endif
 		}
-		__device__ float intersect2(const ray& r, const TraceRecord& rec) const {
-			glm::vec3 t1 = (aabbMin - r.o) / r.d, t2 = (aabbMax - r.o) / r.d;
-			glm::vec3 tmin = glm::min(t1, t2), tmax = glm::max(t1, t2);
-
-			float ttmin = glm::max(glm::max(tmin.x, tmin.y), tmin.z);
-			float ttmax = glm::min(glm::min(tmax.x, tmax.y), tmax.z);
-
-			if (ttmax >= ttmin && ttmin < rec.t && ttmax > 0) {
-				return ttmin;
-			}
-			return 1e10f;
-		}
 	};
 
 	void _updateNodeBounds(std::vector<BVHNode>& nodes, std::vector<Tri>& tris, std::vector<int>& indices, int node_idx);
@@ -77,33 +65,7 @@ public:
 		int tri_count;
 		int nodes_used;
 
-		__device__ bool rec_intersect(const ray& r, TraceRecord& rec, int idx) const {
-			if (idx < 0 || idx >= nodes_used) return false;
-			const BVHNode* node = &d_nodes[idx];
-			if (!node->intersect(r, rec)) return false;
-			if (node->isLeaf()) {
-				for (int i = 0; i < node->triCount; i++)
-					intersect_tri(r, rec, d_tris[d_indices[node->leftFirst + i]]);
-			}
-			else
-			{
-				rec_intersect(r, rec, node->leftFirst);
-				rec_intersect(r, rec, node->leftFirst + 1);
-			}
-		}
 		__device__ bool intersect(const ray& r, TraceRecord& rec) const {
-#if 0
-			const BVHNode* node = &d_nodes[root_idx];
-			if (!node->intersect(r, rec)) return false;
-
-			bool hit_any = false;
-			for (int i = 0; i < tri_count; i++) {
-				hit_any |= intersect_tri(r, rec, d_tris[i]);
-			}
-			return hit_any;
-#elif 0
-			return rec_intersect(r, rec, root_idx);
-#else
 			bool hit_any = false;
 			const BVHNode* nodes[64];
 			int head = 0;
@@ -113,27 +75,9 @@ public:
 				const BVHNode* node = nodes[--head];
 				if (!node->intersect(r, rec)) continue;
 				if (node->isLeaf()) {
-#if 1
 					for (int i = 0; i < node->triCount; i++) {
 						hit_any |= intersect_tri(r, rec, d_tris[d_indices[node->leftFirst + i]]);
 					}
-#else
-					unsigned long long tmp = (unsigned long long)node;
-					int col = 0;
-						
-					col ^= 0xff & (tmp >>  0);
-					col ^= 0xff & (tmp >>  8);
-					col ^= 0xff & (tmp >> 16);
-					col ^= 0xff & (tmp >> 24);
-					col ^= 0xff & (tmp >> 32);
-					col ^= 0xff & (tmp >> 40);
-					col ^= 0xff & (tmp >> 48);
-					col ^= 0xff & (tmp >> 56);
-
-					rec.t = node->intersect2(r, rec);
-					rec.n = glm::vec3(0, col/(float)0xff, 0);
-					return true;
-#endif
 				}
 				else {
 					nodes[head++] = &d_nodes[node->leftFirst + 0];
@@ -142,7 +86,6 @@ public:
 			}
 
 			return hit_any;
-#endif
 		}
 	};
 
@@ -150,89 +93,6 @@ public:
 
 	handle_cu getDeviceHandle() const { return handle_cu{ d_nodes,d_tris,d_indices,root_idx,tri_count,nodes_used }; }
 
-};
-
-struct aabb {
-	glm::vec3 min{1e30f}, max{-1e30f};
-
-	glm::vec3 extents() const { return max - min; }
-	void expand(glm::vec3 p) { min = glm::min(min, p); max = glm::max(max, p); }
-
-	__device__ bool intersect(const ray& r, const TraceRecord& rec) const {
-		glm::vec3 t1 = (min - r.o) / r.d, t2 = (max - r.o) / r.d;
-		glm::vec3 tmin = glm::min(t1, t2), tmax = glm::max(t1, t2);
-
-		float ttmin = glm::max(glm::max(tmin.x, tmin.y), tmin.z);
-		float ttmax = glm::min(glm::min(tmax.x, tmax.y), tmax.z);
-
-		return ttmax >= ttmin && ttmin < rec.t && ttmax > 0;
-	}
-};
-
-class TriangleBVH_2 {
-	struct BVHNode {
-		aabb bounds{};
-		int leftChild_firstTriangle{}, triangle_count{};
-
-		__device__ bool isLeaf() const { return triangle_count > 0; };
-	};
-
-	BVHNode* d_nodes;
-	Tri* d_tris;
-	int* d_indices;
-	int root_node_idx, triangle_count, nodes_used;
-
-	std::vector<BVHNode> nodes;
-	std::vector<Tri> triangles;
-	std::vector<int> indices;
-
-	void _updateNodeBounds(int node_index);
-	void _subdivideNode(int node_index);
-
-	TriangleBVH_2(const TriangleBVH_2&) = delete;
-	TriangleBVH_2& operator=(const TriangleBVH_2&) = delete;
-
-public:
-
-	TriangleBVH_2(TriangleBVH_2&&);
-	TriangleBVH_2& operator=(TriangleBVH_2&&);
-
-	~TriangleBVH_2();
-
-	struct handle_cu {
-		BVHNode* d_nodes;
-		Tri* d_tris;
-		int* d_indices;
-		int root_node_index, triangle_count, nodes_used;
-
-		__device__ bool intersect(const ray& r, TraceRecord& rec) const {
-			bool hit_any = false;
-			const BVHNode* nodes[64];
-			int head = 0;
-			nodes[head++] = &d_nodes[root_node_index];
-
-			while (head > 0) {
-				const BVHNode* node = nodes[--head];
-				if (!node->bounds.intersect(r, rec)) continue;
-				if (node->isLeaf()) {
-					for (int i = 0; i < node->triangle_count; i++) {
-						hit_any |= intersect_tri(r, rec, d_tris[d_indices[node->leftChild_firstTriangle + i]]);
-					}
-					continue;
-				}
-				else {
-					nodes[head++] = &d_nodes[node->leftChild_firstTriangle + 0];
-					nodes[head++] = &d_nodes[node->leftChild_firstTriangle + 1];
-				}
-			}
-
-			return hit_any;
-		}
-	};
-
-	TriangleBVH_2(int triangle_count, int seed);
-
-	handle_cu getDeviceHandle() const { return handle_cu{ d_nodes, d_tris, d_indices, root_node_idx, triangle_count, nodes_used }; }
 };
 
 } // namespace RT_ENGINE
